72a0fda895052ac4bf414bafa6addf448866addb
ArrowDynamics <roboticsbothell@gmail.com>
2025-10-20 23:47:25 +0000

    Basic setup

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	router.Run()
+}
