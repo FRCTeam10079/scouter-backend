11f2383db26292232eb71ecdc819399c21194641
ArrowDynamics <roboticsbothell@gmail.com>
2025-11-11 00:39:15 +0000

    Switch to TypeScript and Prisma ORM

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "single"],
+  },
+};
